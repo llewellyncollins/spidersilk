2eba1a2801ece85d43dd7b78e1105bdb1b857020
llewellyncollins <llewellyn.c.collins@gmail.com>
2020-11-21 23:20:06 +0000

    Update content

--- src/components/AppDevSection/AppDevSection.tsx
+++ src/components/AppDevSection/AppDevSection.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import FeatureBlock from '../FeatureBlock/FeatureBlock';
+import IconDevices from './icons/devices_other-white-48dp.svg';
+import IconHTTPs from './icons/https-white-48dp.svg';
+import IconInsights from './icons/insights-white-48dp.svg';
+import IconMonetization from './icons/monetization_on-white-48dp.svg';
+import IconStar from './icons/star_border-white-48dp.svg';
+import IconDesigns from './icons/design_services-white-48dp.svg';
 import './appDevSection.scss';
 
 export default function AppDevSection() {
@@ -8,31 +14,31 @@ export default function AppDevSection() {
             <h1>App Development</h1>
 
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in erat et ligula vestibulum feugiat. Aenean pharetra nibh non
-                porttitor imperdiet. Donec non nunc at ante sollicitudin gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Sed in erat et ligula vestibulum feugiat. Aenean pharetra nibh non porttitor imperdiet. Donec non nunc at ante sollicitudin
-                gravida.
+                Here at SPIDERSILK, we strive to deliver high-quality applications to our clients. All our applications are thoroughly
+                tested to ensure a secure, performant, and visually stunning experience for your customers.
             </p>
-
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in erat et ligula vestibulum feugiat. Aenean pharetra nibh non
-                porttitor imperdiet. Donec non nunc at ante sollicitudin gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Sed in erat et ligula vestibulum feugiat. Aenean pharetra nibh non porttitor imperdiet. Donec non nunc at ante sollicitudin
-                gravida.
+                We treat your customers as if they were our customers. We work with our clients to tailor the user experience and encourage
+                maximum in-application engagement. An engaged user is a potential paying user. Using custom metrics we can help you identify
+                what is working well and what can be improved.
+            </p>
+            <p>
+                If you would like to see your application transition from inception to fruition? Please don't hesitate to reach out now. We
+                will get back to you as soon as possible with a plan to get your application into your customer's hands!
             </p>
 
             <div className='feature-blocks'>
                 <div className='feature-blocks-row'>
-                    <FeatureBlock title='Cross Platform' icon='devices_other-white-48dp' />
+                    <FeatureBlock title='High Quality' icon={IconStar} />
                 </div>
                 <div className='feature-blocks-row'>
-                    <FeatureBlock title='Cross Platform' icon='devices_other-white-48dp' />
-                    <FeatureBlock title='Cross Platform' icon='devices_other-white-48dp' />
+                    <FeatureBlock title='Monetization' icon={IconMonetization} />
+                    <FeatureBlock title='Analytics' icon={IconInsights} />
                 </div>
                 <div className='feature-blocks-row'>
-                    <FeatureBlock title='Cross Platform' icon='devices_other-white-48dp' />
-                    <FeatureBlock title='Cross Platform' icon='devices_other-white-48dp' />
-                    <FeatureBlock title='Cross Platform' icon='devices_other-white-48dp' />
+                    <FeatureBlock title='Cross Platform' icon={IconDevices} />
+                    <FeatureBlock title='Secure' icon={IconHTTPs} />
+                    <FeatureBlock title='UI / UX' icon={IconDesigns} />
                 </div>
             </div>
         </section>
--- src/components/WebDevSection/WebDevSection.tsx
+++ src/components/WebDevSection/WebDevSection.tsx
@@ -4,21 +4,21 @@ import './webDevSection.scss';
 export default function WebDevSection() {
     return (
         <section className='web-dev-section'>
-            <h1>App Development</h1>
+            <h1>Website Development</h1>
 
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in erat et ligula vestibulum feugiat. Aenean pharetra nibh non
-                porttitor imperdiet. Donec non nunc at ante sollicitudin gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Sed in erat et ligula vestibulum feugiat. Aenean pharetra nibh non porttitor imperdiet. Donec non nunc at ante sollicitudin
-                gravida.
+                In today's fast-paced world, creating a cookie-cutter website no longer meets the brief for businesses trying to stay ahead
+                of their competition. Your website is your potential customer's first point of engagement with your business. At SPIDERSILK
+                we ensure that we capitalize on every customer interaction. By providing your customers with an intuitive and accessible
+                user experience, astonishingly fast load times, and a secure browsing experience.
             </p>
 
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in erat et ligula vestibulum feugiat. Aenean pharetra nibh non
-                porttitor imperdiet. Donec non nunc at ante sollicitudin gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Sed in erat et ligula vestibulum feugiat. Aenean pharetra nibh non porttitor imperdiet. Donec non nunc at ante sollicitudin
-                gravida.
+                Not only do we provide your customers with a gratifying experience, but we also aim to maximize your business's web
+                presence. By using the SEO indexing algorithms to our advantage, ensuring your website meets the stringent requirements to
+                get it up the SEO page rankings.
             </p>
+            <p>If you aspire to take your business to further heights, please get in touch using the contact form below. </p>
         </section>
     );
 }
