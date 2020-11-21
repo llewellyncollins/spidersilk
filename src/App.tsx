aa62c7616916ddcec6d37299f9b223c49b6cc56f
llewellyncollins <llewellyn.c.collins@gmail.com>
2020-11-21 00:34:27 +0000

    Remove remote config

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,19 @@ import AppDevSection from './components/AppDevSection/AppDevSection';
 import ContactForm from './components/ContactForm/ContactForm';
 import Nav from './components/Nav/Nav';
 import WebDevSection from './components/WebDevSection/WebDevSection';
-import useRemoteConfig from './hooks/useRemoteConfig';
 import './styles/main.scss';
 
 function App() {
-    const config = useRemoteConfig();
-
     return (
         <div className='App'>
             <Nav />
-            {config ? (
-                <div className='content'>
-                    <AppDevSection />
-                    <section className='side-bar'>
-                        <WebDevSection />
-                        <ContactForm />
-                    </section>
-                </div>
-            ) : (
-                <div>Loading...</div>
-            )}
+            <div className='content'>
+                <AppDevSection />
+                <section className='side-bar'>
+                    <WebDevSection />
+                    <ContactForm />
+                </section>
+            </div>
         </div>
     );
 }
--- src/localFirebase.ts
+++ src/localFirebase.ts
@@ -2,7 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/analytics';
 import 'firebase/performance';
-import 'firebase/remote-config';
 
 const firebaseApp = firebase.initializeApp( {
     apiKey: "AIzaSyBvGbY1UvVOod28gG7OeeQAl0IEg0EdIXU",
@@ -18,9 +17,4 @@ const firebaseApp = firebase.initializeApp( {
 export const performance = firebaseApp.performance();
 export const analytics = firebaseApp.analytics();
 export const firestore = firebaseApp.firestore();
-export const remoteConfig = firebaseApp.remoteConfig();
 
-remoteConfig.settings = {
-    minimumFetchIntervalMillis: 43200000,
-    fetchTimeoutMillis: 600000
-};
